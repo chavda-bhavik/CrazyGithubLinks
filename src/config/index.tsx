eaef66aab20270644d69eab7d8fb6b5a64646a05
chavda-bhavik <bhavik.chvda@gmail.com>
2021-08-20 16:15:13 +0530

    updated siteUrl

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -2,7 +2,10 @@ const siteMetadata = {
     title: `CrazyGithubLinks`,
     description: `CrazyGithubLinks is a place to create unique sharable links for your repositories, that encourages its users to star and view the repository.`,
     image: `/og/default.png`,
-    siteUrl: 'http://localhost:3000',
+    siteUrl:
+        process.env.NODE_ENV === 'production'
+            ? 'https://crazygithublinks.herokuapp.com/'
+            : 'http://localhost:3000',
 };
 
 export { siteMetadata };
